c99b94396b4809075b70f2c082a1e63efa9ce01a
Łukasz Dudek <lukaszdudek@ukaszs-MacBook-Air.local>
2022-06-22 11:05:22 +0200

    fix: typo Serch -> Search

--- src/modules/search/services/search.service.ts
+++ src/modules/search/services/search.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { SerchResult } from '../searchResult';
+import { SearchResult } from '../searchResult';
 
 @Injectable()
 export class SearchService {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  async search(query: string): Promise<SerchResult[]> {
+  async search(query: string): Promise<SearchResult[]> {
     return [];
   }
 }
